c59f16f786fe32104b0bb4b383e0b990bee5c691
kakashi <john.cha@qq.com>
2022-07-25 23:34:28 +0800

    add Pause、Resume、Destroy support to UnityIOCContainer

--- Unity-IOC-Unity/Assets/IO.Unity3D.Source/IOC-Unity/Runtime/Components/UnityIOCContainerMono.cs
+++ Unity-IOC-Unity/Assets/IO.Unity3D.Source/IOC-Unity/Runtime/Components/UnityIOCContainerMono.cs
@@ -11,8 +11,9 @@ namespace IO.Unity3D.Source.IOCUnity
     // @Email: john.cha@qq.com
     // @Date: 2022-05-09 01:12
     //******************************************
-    internal class UnityIOCContainerMono : MonoBehaviour
+    public class UnityIOCContainerMono : MonoBehaviour
     {
+
         private UnityIOCContainer _UnityIOCContainer;
         
         private IReadOnlyList<IUnityUpdate> _UnityUpdates= new List<IUnityUpdate>(0);
@@ -21,6 +22,8 @@ namespace IO.Unity3D.Source.IOCUnity
         private IReadOnlyList<IUnityApplication> _UnityApplications= new List<IUnityApplication>(0);
         private IReadOnlyList<IUnityEditor> _UnityEditors= new List<IUnityEditor>(0);
         private IReadOnlyList<IUnityGUI> _UnityGUIs= new List<IUnityGUI>(0);
+
+        private PauseOrResume _Pause = PauseOrResume.None;
 
         private void Awake()
         {
@@ -41,6 +44,11 @@ namespace IO.Unity3D.Source.IOCUnity
 
         private void Update()
         {
+            if ((_Pause & PauseOrResume.Update) != 0)
+            {
+                return;
+            }
+
             for (int i = 0; i < _UnityUpdates.Count; i++)
             {
                 _UnityUpdates[i].Update();
@@ -49,6 +57,10 @@ namespace IO.Unity3D.Source.IOCUnity
 
         private void LateUpdate()
         {
+            if ((_Pause & PauseOrResume.LateUpdate) != 0)
+            {
+                return;
+            }
             for (int i = 0; i < _UnityLateUpdates.Count; i++)
             {
                 _UnityLateUpdates[i].LateUpdate();
@@ -57,6 +69,10 @@ namespace IO.Unity3D.Source.IOCUnity
         
         private void FixedUpdate()
         {
+            if ((_Pause & PauseOrResume.FixedUpdate) != 0)
+            {
+                return;
+            }
             for (int i = 0; i < _UnityFixedUpdates.Count; i++)
             {
                 _UnityFixedUpdates[i].FixedUpdate();
@@ -76,14 +92,6 @@ namespace IO.Unity3D.Source.IOCUnity
             for (int i = 0; i < _UnityApplications.Count; i++)
             {
                 _UnityApplications[i].OnApplicationQuit();
-            }
-        }
-
-        private void OnApplicationFocus(bool hasFocus)
-        {
-            for (int i = 0; i < _UnityApplications.Count; i++)
-            {
-                _UnityApplications[i].OnApplicationFocus(hasFocus);
             }
         }
 
@@ -123,5 +131,25 @@ namespace IO.Unity3D.Source.IOCUnity
         {
             _UnityIOCContainer.IOCContainer.Destroy();
         }
+        
+        public void Pause(PauseOrResume pauseOrResume = PauseOrResume.All)
+        {
+            _Pause |= pauseOrResume;
+        }
+
+        public void Resume(PauseOrResume pauseOrResume = PauseOrResume.All)
+        {
+            _Pause = (_Pause & (~pauseOrResume));
+        }
+    }
+    
+    [Flags]
+    public enum PauseOrResume
+    {
+        None        = 0,
+        Update      = 1 << 0,
+        LateUpdate  = 1 << 1,
+        FixedUpdate = 1 << 2,
+        All         = Update | LateUpdate | FixedUpdate,
     }
 }
